ef11ee2744041c35106869d5959d44fbd724a8dc
Aaron Lisker <aelisker@gmail.com>
2020-04-06 17:25:58 -0700

    added bootstrap classes for past due and near dates

--- assets/js/script.js
+++ assets/js/script.js
@@ -13,6 +13,8 @@ var createTask = function(taskText, taskDate, taskList) {
   // append span and p element to parent li
   taskLi.append(taskSpan, taskP);
 
+  //check due date
+  auditTask(taskLi);
 
   // append to ul list on the page
   $("#list-" + taskList).append(taskLi);
@@ -43,6 +45,29 @@ var loadTasks = function() {
 
 var saveTasks = function() {
   localStorage.setItem("tasks", JSON.stringify(tasks));
+};
+
+var auditTask = function(taskEl) {
+  //get date from task element
+  var date = $(taskEl).find("span").text().trim();
+  //to ensure element is getting to function
+  console.log(date);
+
+  //convert date to moment object at 5:00pm
+  var time = moment(date, "L").set("hour", 17);
+  //this should print out an object for the value of the date variable, but at 5pm that day
+  console.log(time);
+
+  //remove any old classes from element
+  $(taskEl).removeClass("list-group-item-warning list-group-item-danger");
+
+  //apply new class if task is near/over due date
+  if (moment().isAfter(time)) {
+    $(taskEl).addClass("list-group-item-danger");
+  }
+  else if (Math.abs(moment().diff(time, "days")) <=2) {
+    $(taskEl).addClass("list-group-item-warning");
+  }
 };
 
 $(".list-group").on("click", "p", function() {
@@ -142,6 +167,9 @@ $(".list-group").on("change", "input[type='text']", function() {
 
   //replace input with span element
   $(this).replaceWith(taskSpan);
+
+  //pass task's <li> element into auditTask() to check new due date
+  auditTask($(taskSpan).closest(".list-group-item"));
 });
 
 //make card and ul sortable
@@ -224,7 +252,7 @@ $("#task-form-modal").on("shown.bs.modal", function() {
 
 //datepicker
 $("#modalDueDate").datepicker({
-  minDate: 1
+  minDate: 0
 });
 
 // save button in modal was clicked
